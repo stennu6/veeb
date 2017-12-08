8100d30f931f53224514c92467c2f63d60684a80
Sten Elisson <sten.elisson@khk.ee>
2017-12-08 09:27:39 +0200

    Fikseeritud projekti nimetus Kirjeldatud kommentaaride kasutamine

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stenelisson
+ * Date: 12/8/17
+ * Time: 9:03 AM
+ */
+
+# see on ka üherealine kommentaar
+// üherealine kommentaar
+/**
+ * mitme-
+ * realine
+ * kommentaar
+ * see on veel üks rida
+ */
+
+?>
