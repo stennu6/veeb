534604bb1b01a36784568c42fc90acd953b5feee
Sten Elisson <sten.elisson@khk.ee>
2017-12-14 14:56:01 +0200

    tabeli loomine lisa tekst

--- funktsioonid.php
+++ funktsioonid.php
@@ -10,10 +10,10 @@ function tabel ($sona1, $sona2, $sona3, $sona4){
         for ($rida = 1; $rida <= 4; $rida++){
             echo '<tr>';
                 echo '<td>';
-            echo ${'sona'}.$rida;
+            echo ${'sona'.$rida};
+        };
                 echo '</td>';
             echo '</tr>';
-        }
     echo '</table>';
 }
 tabel ('Kui rohkem küsimusi','pole', 'siis ma võin','lubada teid koju');
