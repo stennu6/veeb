eb5c94f72c376bcfcc44c9d86d655a6f0a12fb20
Sten Elisson <sten.elisson@khk.ee>
2017-12-14 13:32:52 +0200

    Numbrid värviliseks

--- numbrid.php
+++ numbrid.php
@@ -5,8 +5,12 @@
  * Date: 12/14/17
  * Time: 12:26 PM
  */
-$red = array(1, 3, 5, 7, 9);
-$blue = array(2, 4, 6, 8, 10);
-echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . "."
-echo
+for ($arv = 1; $arv <= 10; $arv++) {
+    if ($arv % 2 == 0){
+        $varv = 'blue'
+    } else {
+        $varv = 'red'
+    }
+    echo '<font color="'.$varv.' " > '.$arv.'</font><br />';
+}
 ?>
