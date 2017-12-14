232d0f823b0afa6037611c1e47e83ca66dd24cbd
Sten Elisson <sten.elisson@khk.ee>
2017-12-14 14:48:34 +0200

    tabeli loomine lisa

--- funktsioonid.php
+++ funktsioonid.php
@@ -10,7 +10,7 @@ function tabel ($sona1, $sona2, $sona3, $sona4){
         for ($rida = 1; $rida <= 4; $rida++){
             echo '<tr>';
                 echo '<td>';
-            echo ${'sona'}.$$rida;
+            echo ${'sona'}.$rida;
                 echo '</td>';
             echo '</tr>';
         }
