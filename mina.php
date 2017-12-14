20e51d3c3c0612ae46a294e28d3932adf623f79a
Sten Elisson <sten.elisson@khk.ee>
2017-12-14 13:25:23 +0200

    Nimed delfineerimine

--- mina.php
+++ mina.php
@@ -8,4 +8,10 @@
  */
 $sten = 'Sten';
 echo 'Minu nimi on'.$sten.'<br />';
+/**
+ *
+ */
+define('MINU_NIMI', 'Sten');
+MINU_NIMI = 'Stan'
+echo 'Minu nimi on'.MINU_NIMI.'<br />';
 ?>
--- numbrid.php
+++ numbrid.php
@@ -5,5 +5,8 @@
  * Date: 12/14/17
  * Time: 12:26 PM
  */
-
+$red = array(1, 3, 5, 7, 9);
+$blue = array(2, 4, 6, 8, 10);
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . "."
+echo
 ?>
