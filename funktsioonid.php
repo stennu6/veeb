229ba6476d5c230a22a98655f32b3503fae63186
Sten Elisson <sten.elisson@khk.ee>
2017-12-14 14:38:38 +0200

    tabeli loomine

---
+++ funktsioonid.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stenelisson
+ * Date: 12/14/17
+ * Time: 2:31 PM
+ */
+function tabel ($sona1, $sona2, $sona3, $sona4){
+    echo '<table border="1">';
+        for ($rida = 1; $rida <= 4; $rida++){
+            echo '<tr>';
+                echo '<td>';
+            //echo $sona1 jne
+                echo '<td>';
+            echo '<tr>';
+        }
+    echo '</table>';
+}
+tabel ('1','2', '3','4');
+?>
