0b81766c2506743d014090a249a8f929010e9fd5
Sten Elisson <sten.elisson@khk.ee>
2017-12-14 13:50:47 +0200

    Väljastatud must-valge tekst

--- varv.php
+++ varv.php
@@ -5,7 +5,16 @@
  * Date: 12/14/17
  * Time: 1:35 PM
  */
-for($kord = 1; $kord <= 5; $kord++){
-    echo 'Värviline tekst >br />';
+for($rida = 1; $rida <= 5; $rida++){
+    $varv ='#';
+    for ($kord = 1; $kord <=6; $kord++){
+        $juhuarv = rand(0, 15);
+        $symbol = dexchex($juhuarv);
+        $varv = $varv.$symbol;
+    }
+    echo '<font color="'.$varv.'">'.
+        'Värviline tekst'.
+        '</font>'.
+        '<br />';
 }
 ?>
