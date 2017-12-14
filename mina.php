7a02c80cdb11122e87f4f2f82a106f4849f5d2ce
Sten Elisson <sten.elisson@khk.ee>
2017-12-14 12:29:40 +0200

    Numbrid

---
+++ mina.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stenelisson
+ * Date: 12/14/17
+ * Time: 12:09 PM
+ */
+echo "Minu nimi on Sten"
+
+
+?>
---
+++ numbrid.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stenelisson
+ * Date: 12/14/17
+ * Time: 12:26 PM
+ */
+
+?>
