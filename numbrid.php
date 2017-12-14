96d9d8cdac8da170050bacf9aded28c2e954eae8
Sten Elisson <sten.elisson@khk.ee>
2017-12-14 14:06:58 +0200

    Väljastatud must-valge tekst

--- numbrid.php
+++ numbrid.php
@@ -7,10 +7,29 @@
  */
 for ($arv = 1; $arv <= 10; $arv++) {
     if ($arv % 2 == 0){
-        $varv = 'blue'
-    } else {
-        $varv = 'red'
+        $varv = 'blue';
     }
-    echo '<font color="'.$varv.' " > '.$arv.'</font><br />';
+    else{
+        $varv = 'red';
+    }
+    echo '<font color="'.$varv.' " > '.$arv
+        .'</font><br />';
 }
+
+//
+$a = 5; $b = 2;
+$c = $a++;
+$d = $b--;
+echo 'a = '.$a.'<br />';
+echo 'b = '.$b.'<br />';
+echo 'c = '.$c.'<br />';
+echo 'd = '.$d.'<br />';
+//
+$a = 5; $b = 2;
+$c = ++$a;
+$d = --$b;
+echo 'a = '.$a.'<br />';
+echo 'b = '.$b.'<br />';
+echo 'c = '.$c.'<br />';
+echo 'd = '.$d.'<br />';
 ?>
--- varv.php
+++ varv.php
@@ -5,6 +5,7 @@
  * Date: 12/14/17
  * Time: 1:35 PM
  */
+header('Refresh: 1');
 for($rida = 1; $rida <= 5; $rida++){
     $varv ='#';
     for ($kord = 1; $kord <=6; $kord++){
